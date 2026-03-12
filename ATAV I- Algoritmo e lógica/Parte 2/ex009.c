819f8ccf2c64171f4d9e8fe1d311d0010fb7b53c
Pedro Giacciani <giaccianipedro@gmail.com>
2026-03-11 21:32:56 -0300

    update

--- ATAV I- Algoritmo e lógica/Parte 2/ex009.c
+++ ATAV I- Algoritmo e lógica/Parte 2/ex009.c
@@ -1,1 +1,14 @@
-//Depois eu faço
+#include <stdio.h>
+
+int main(){
+    int idd, maior, menor;
+    printf("Digite a idade do primeiro filho:\n");
+    scanf("%d", &idd);
+    maior = idd;
+    menor = idd;
+
+    for  (int i = 0; i <= 5; i++){
+        printf("")
+    }
+    
+}
