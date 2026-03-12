5385da0f8d7707a6989773b17624447697cd0add
Pedro Giacciani <giaccianipedro@gmail.com>
2026-03-11 22:07:17 -0300

    ex009

--- ATAV I- Algoritmo e lógica/Parte 2/ex009.c
+++ ATAV I- Algoritmo e lógica/Parte 2/ex009.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main(){
+    setlocale(LC_ALL, "portuguese");
+
     int idd, maior, menor;
     printf("Digite a idade do primeiro filho:\n");
     scanf("%d", &idd);
     maior = idd;
     menor = idd;
 
-    for  (int i = 0; i <= 5; i++){
-        printf("")
+    for(int i = 1; i <= 4; i++){
+        printf("Digite a idade do proximo filho");
+        scanf("%d", &idd);
+        
+        if (idd > maior){
+            maior = idd;
+        } else if (idd < menor){
+            idd = menor;
+        }   
     }
+
+    printf("O filho mais velho tem %d, e o mais novo tem %d", maior, menor);
+
+    return 0;
     
 }
