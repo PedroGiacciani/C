160ba3606838a155a8cd13a67dbbdfd64534dea6
Pedro Giacciani <giaccianipedro@gmail.com>
2026-03-11 22:22:17 -0300

    update

--- ATAV I- Algoritmo e lógica/Parte 2/ex008.c
+++ ATAV I- Algoritmo e lógica/Parte 2/ex008.c
@@ -1,1 +1,15 @@
-//Depois eu faço
+#include <stdio.h>
+
+int main(){
+    int n, s;
+
+    for (int i = 1; i <= 3; i++){
+        printf("Digite um numero:\n");
+        scanf("%d", &n);
+        s += n;
+    }
+
+    printf("A soma desses 3 numeros eh %d", s);
+    return 0;
+
+}
