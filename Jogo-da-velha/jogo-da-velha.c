b2fb8c3406ff0283f34175a92316ff7f6ab46612
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-03-20 12:19:54 -0300

    update

--- Jogo-da-velha/jogo-da-velha.c
+++ Jogo-da-velha/jogo-da-velha.c
@@ -8,25 +8,26 @@ int main (){
     int jogadas = 0;
     int escolha;
 
-    while (jogadas < 9 && ganhou != 0){
-        system('cls');
+    while(jogadas < 9 && ganhou == 0){
+        system("cls");
 
-        printf("[%c] [%c] [%c]", p1, p2, p3);
-        printf("[%c] [%c] [%c]", p4, p5, p6);
-        printf("[%c] [%c] [%c]", p7, p8, p9);
+        printf("JOGO DA VELHA\n");
+        printf("[%c] [%c] [%c]\n", p1, p2, p3);
+        printf("[%c] [%c] [%c]\n", p4, p5, p6);
+        printf("[%c] [%c] [%c]\n", p7, p8, p9);
 
         printf("Escolha uma casa\n");
         scanf("%d", &escolha);
 
-        if(p1 == 1 && escolha == 1){p1 = jogador;}
-        if(p2 == 2 && escolha == 2){p2 = jogador;}
-        if(p2 == 3 && escolha == 3){p3 = jogador;}
-        if(p2 == 4 && escolha == 4){p4 = jogador;}
-        if(p2 == 5 && escolha == 5){p5 = jogador;}
-        if(p2 == 6 && escolha == 6){p6 = jogador;}
-        if(p2 == 7 && escolha == 7){p7 = jogador;}
-        if(p2 == 8 && escolha == 8){p8 = jogador;}
-        if(p2 == 9 && escolha == 9){p9 = jogador;}
+        if(p1 == '1' && escolha == 1){p1 = jogador;}
+        else if(p2 == '2' && escolha == 2){p2 = jogador;}
+        else if(p3 == '3' && escolha == 3){p3 = jogador;}
+        else if(p4 == '4' && escolha == 4){p4 = jogador;}
+        else if(p5 == '5' && escolha == 5){p5 = jogador;}
+        else if(p6 == '6' && escolha == 6){p6 = jogador;}
+        else if(p7 == '7' && escolha == 7){p7 = jogador;}
+        else if(p8 == '8' && escolha == 8){p8 = jogador;}
+        else if(p9 == '9' && escolha == 9){p9 = jogador;}
         else{
             printf("[ERRO] Numero invalido ou ocupado. Pressione Enter para continuar");
             getchar(); getchar();
@@ -42,17 +43,23 @@ int main (){
         if(p1 == p5 && p5 == p9){ganhou = 1;}       
         if(p3 == p5 && p5 == p7){ganhou = 1;}       
         
-        if (ganhou == 1)
+        if (ganhou != 1)
         {
-            printf("O %d venceu. Parabens!", jogador);
-        } else{
             if (jogador =='X')
             {
                 jogador = 'O';
             }else{
                 jogador = 'X';
             }
+        } else{
+            printf("O %c venceu. Parabens!", jogador);
         }
+
+        if (jogadas == 8)
+        {
+            printf("Deu velha !");
+        }
+        
         
         jogadas++;
     }
