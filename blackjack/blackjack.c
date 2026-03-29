8cf9a23f210278dd602d08c7ba8dd754c2acc352
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-03-29 15:12:51 -0300

    Blackjack

--- blackjack/blackjack.c
+++ blackjack/blackjack.c
@@ -3,18 +3,54 @@
 #include <time.h>
 
 int main(){
-    //Declaração de variavéis
-    int c1 = 0, c2 = 0, fim = 0;
-    srand(time(NULL));
+    int soma1 = 0, soma2 = 0; //Declaração de variavéis
+    srand(time(NULL)); //Definindo uma nova sequência de números aleatórios
+    printf("BLACKJACK\nClique Enter para jogar");
+    getchar();
 
-    for (int i = 0; i < 21 && fim == 0; i++){
-        system("cls");
+    //Jogadas do jogador
+    for (int i = 0; i < 21; i++){
+        system("cls"); //Limpar terminal
+    
+        int c1 = 0, op; //Variavéis usadas apenas pelo jogador
+        printf("BLACKJACK\n");
 
-        printf("BLACK JACK\nClique Enter para jogar!");
-        getchar();
+        c1 = rand() % 11 + 1; //Pegar cartar aleatória
+        soma1 += c1; //Armazenar valor de carta aleatória
+        
+        printf("Carta atual: %d\n", c1);
+        printf("Soma das cartas: %d\n", soma1);
 
-        printf("Sua carta: %d", c1);
-        getchar();
+        printf("1 - Pegar mais uma carta\n2 - Parar\n");
+        scanf("%d", &op);
+
+        //Condicional de parar a jogada
+        if (op == 2){
+            i = 21;
+        }  
+    }
+
+    //Jogadas da máquina
+    while (soma2 < 20)
+    {
+        int c2; //Variavéis da máquina
+        c2 = rand() % 11 + 1; //Pegar carta aleatória
+        soma2 += c2; //Armazenar carta aleatória
     }
     
+    printf("Cartas da mesa: %d\n", soma2);
+
+    //Condições de vitória 
+    if ((soma1 == 21) || (soma1 < 21 && soma1 > soma2) || (soma1 > 21 && soma1 < soma2) || (soma1 < 21 && soma2 > 21))
+    {
+        printf("O jogador Venceu. Parabens");
+    } else if ((soma2 == 21) || (soma2 < 21 && soma2 > soma1) || (soma2 > 21 && soma2 < soma1) || (soma2 < 21 && soma1 > 21))
+    {
+        printf("A mesa te superou. Perdedor");
+    } else if (soma1 == soma2)
+    {
+        printf("O jogo empatou");
+    }
+
+    return 0;
 }
