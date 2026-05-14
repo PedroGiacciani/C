208f064bad5c6a390647f78db8c07aa176a2a0f7
Pedro Giacciani <91699780+PedroGiacciani@users.noreply.github.com>
2026-05-14 01:05:22 -0300

    Exercício de array

--- ATAV - Algoritmo e lógica/Segundo bimestre/ex013.c
+++ ATAV - Algoritmo e lógica/Segundo bimestre/ex013.c
@@ -1,1 +1,14 @@
-//Depois eu faço
+#include <stdio.h>
+
+int main(){
+    int idade[50], i;
+    for(i = 0; i < 50; i++){
+        printf("Digite a idade da pessoa %d\n", i + 1);
+        scanf("%d", &idade[i]);
+    }
+    for(i = 0; i < 50; i++){
+        printf("Idade da pessoa %d: %d\n", i + 1, idade[i]);
+    }
+
+    return 0;
+}
